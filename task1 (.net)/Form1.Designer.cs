8cf20260346ed0a75818643bfc0b4c5223daeab5
cherryyjuicee <67759831+cherryyjuicee@users.noreply.github.com>
2021-09-27 14:41:35 +0300

    Add files via upload

---
+++ task1 (.net)/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace task1__.net_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 89);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(557, 234);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(305, 329);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Произвольная\r\nматрица";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(441, 61);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(446, 329);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 51);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Решение";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(144, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(286, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Количество строк = Количество столбцов";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(156, 329);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 51);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Своя матрица";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 329);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(123, 51);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Пример";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(581, 34);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Найти скалярное произведение строки и столбца, соответствующих строке с первым \r\n" +
+    "найденным отрицательным элементом и последним нулевым.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 404);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Скалярное произведение матрицы A(n*n)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ task1 (.net)/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace task1__.net_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            int n;
+            
+            int.TryParse(textBox2.Text, out n);
+            
+            int[,] arr1 = new int[n, n];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    arr1[i, j] = rnd.Next(-30, 30);
+            }
+
+
+            
+            if (n <= 0)
+            {
+                MessageBox.Show("Введите количество строк и столбцов (>0)");
+            }
+
+            else
+            {
+                dataGridView1.RowCount = n;
+                dataGridView1.ColumnCount = n;
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        dataGridView1.Rows[i].Cells[j].Value = arr1[i, j].ToString();
+                    }
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            {
+                int x = -1;
+                int y = -1;
+
+                int n;
+                
+                int.TryParse(textBox2.Text, out n);
+                int[,] arr1 = new int[n, n];
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        arr1[i, j] = int.Parse(dataGridView1.Rows[i].Cells[j].Value.ToString());
+
+                        if (arr1[i, j] < 0 && x == -1)
+                        {
+                            x = i; //номер строки первого отрицательного элемента
+                        }
+                        if (arr1[i, j] == 0)
+                        {
+                            y = j; //номер столбца последнего нулевого элемента
+                        }
+                    }
+                }
+
+                int scl = 0;
+
+                if (x == -1 && y == -1)
+                {
+                    MessageBox.Show("В матрице нет отрицательных и нулевых элементов");
+                }
+                else if (x == -1)
+                {
+                    MessageBox.Show("В матрице нет отрицательных элементов");
+                }
+                else if (y == -1)
+                {
+                    MessageBox.Show("В матрице нет нулевых элементов");
+                }
+                else
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        scl = scl + arr1[x, i] * arr1[i, y];
+                    }
+                    MessageBox.Show("Номер строки с первым отрицательным элементом: " + (x+1) + "\nНомер столбца с последним нулевым элементом: " + (y+1) + "\nСкалярное произведение строки на столбец: " + scl);
+                    
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            int.Parse(textBox2.Text = "4");
+
+
+            int[,] arr1 = new int[4, 4]
+            {
+                {1, 2, 3, 4},
+                {0, 5, 5, 0},
+                {-1, 4, 5, 0},
+                {-5, -8, 0, 5}
+            };
+                       
+            dataGridView1.RowCount = 4;
+            dataGridView1.ColumnCount = 4;
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = arr1[i, j].ToString();
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int n;
+            int.TryParse(textBox2.Text, out n);
+
+            
+            if (n <= 0)
+            {
+                MessageBox.Show("Введите количество строк и столбцов (>0)");
+            }
+
+            else
+            {
+                dataGridView1.RowCount = n;
+                dataGridView1.ColumnCount = n;
+
+            }
+
+        }
+    
+    }
+}
